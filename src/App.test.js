40d13f9cbfcaac2fdb98245aecd6b3a8d03e99b5
arunsingh3586 <arunsingh3586@gmail.com>
2026-02-08 22:02:30 +0530

    Improve axios mocking in App.test.js
    Refactor axios mock to use mockResolvedValue for clarity.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-jest.mock('axios', () => ({
-  get: jest.fn(() => Promise.resolve({ data: [] }))
-}));
+import axios from 'axios';
+jest.mock('axios');
+
+axios.get.mockResolvedValue({
+  data: []
+});
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
